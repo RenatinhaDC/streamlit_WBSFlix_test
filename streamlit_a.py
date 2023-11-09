70c171f9990992b46ad12000f89ba18d9e68f573
RenatinhaDC <143716639+RenatinhaDC@users.noreply.github.com>
2023-11-09 21:17:04 +0100

    Add files via upload

--- streamlit_a.py
+++ streamlit_a.py
@@ -1,26 +1,29 @@
 # Import necessary libraries
-import streamlit as st
 import pandas as pd
+import streamlit as st
 
-# Defining the recommendation function
+# Function to get top recommendations
+def get_top_n_recommendations(movie_title, n, movies_cosines_matrix, user_movie_matrix, movie_ratings_tags):
+    # Find the movieId for the given movie title
+    title_column_name = 'title'
 
-def get_top_n_recommendations(title, n, movies_cosines_matrix, user_movie_matrix, movie_ratings_tags):
-    # Finding the movieId for the given movie title
-    movieId = movie_ratings_tags[movie_ratings_tags['title'].str.contains(title, case=False)]['movieId'].values[0]
+    # Check if the title column exists in the DataFrame
+    if title_column_name not in movie_ratings_tags.columns:
+        raise KeyError(f"Column '{title_column_name}' not found in movie_ratings_tags DataFrame.")
 
-    # Geting the index corresponding to the movieId
-    movie_index = movies_cosines_matrix.index.get_loc(movieId)
+    # Find the movieId for the given movie title
+    movieId = movie_ratings_tags[movie_ratings_tags[title_column_name].str.contains(movie_title, case=False)]['movieId'].values[0]
 
-    # Creating a DataFrame using the values from 'movies_cosines_matrix' for the input 'movieId'
-    movie_cosines_df = pd.DataFrame(movies_cosines_matrix.iloc[movie_index])
+    # Creating a DataFrame using the values from 'movies_cosines_matrix' for the input 'movieId'.
+    movie_cosines_df = pd.DataFrame(movies_cosines_matrix.iloc[movies_cosines_matrix.index.get_loc(movieId)])
 
     # Renaming the column to 'cosine_similarity'
     movie_cosines_df = movie_cosines_df.rename(columns={movieId: 'cosine_similarity'})
 
-    # Remove the row with the index 'movieId'
+    # Removing the row with the index 'movieId'
     movie_cosines_df = movie_cosines_df[movie_cosines_df.index != movieId]
 
-    # Sorting the DataFrame by the 'cosine_similarity' column in descending order
+    # Sorting the DataFrame by the 'cosine_similarity' column in descending order.
     movie_cosines_df = movie_cosines_df.sort_values(by="cosine_similarity", ascending=False)
 
     # Finding out the number of users who rated both the input movie and the other movie
@@ -32,10 +35,10 @@ def get_top_n_recommendations(title, n, movies_cosines_matrix, user_movie_matrix
     # Creating a column for the number of users who rated both movies
     movie_cosines_df['users_who_rated_both_movies'] = no_of_users_rated_both_movies
 
-    # Removing recommendations that have less than 20 users who rated both movies
+    # Removing recommendations that have less than 20 users who rated both movies.
     movie_cosines_df = movie_cosines_df[movie_cosines_df["users_who_rated_both_movies"] > 20]
 
-    # Geting the titles of the recommended movies
+    # Getting the titles of the recommended movies
     recommended_movie_titles = movie_ratings_tags.loc[movie_cosines_df.index, "title"]
 
     # Creating a DataFrame with unique titles
@@ -50,45 +53,22 @@ def get_top_n_recommendations(title, n, movies_cosines_matrix, user_movie_matrix
 
     return top_n_recommendations
 
-# my data
-movies_cosines_matrix = pd.DataFrame()
-user_movie_matrix = pd.DataFrame()
-movie_ratings_tags = pd.DataFrame()
+# Sample DataFrames (Replace with your actual data)
+movies_cosines_matrix = pd.DataFrame()  # Replace with your actual DataFrame
+user_movie_matrix = pd.DataFrame()  # Replace with your actual DataFrame
+movie_ratings_tags = pd.DataFrame()  # Replace with your actual DataFrame
 
 # Streamlit App
 st.title("WBSFlix Recommender")
+st.write("The best movie recommendations for you!")
 
-# Sidebar for user input
-title = st.sidebar.text_input("What is your favorite movie", "Toy Story")
-
-# Main area to display recommendations
-st.header("Your Recommendations")
-
-# Display user's favorite movie
+# Input for user's favorite movie
+title = st.text_input("What is your favorite movie")
 st.write("Your favorite movie is:", title)
 
-# Add a button to trigger the recommendation process
-if st.button("Get Recommendations"):
-    # Debugging: Print the contents of movie_ratings_tags
-    st.write("Contents of movie_ratings_tags:")
-    st.write(movie_ratings_tags)
+# Call the recommendation function
+recommendations = get_top_n_recommendations(title, 5, movies_cosines_matrix, user_movie_matrix, movie_ratings_tags)
 
-    try:
-        # Call your recommendation function
-        recommendations = get_top_n_recommendations(
-            title=title,
-            n=5,  # Replace with the desired number of recommendations
-            movies_cosines_matrix=movies_cosines_matrix,
-            user_movie_matrix=user_movie_matrix,
-            movie_ratings_tags=movie_ratings_tags
-        )
-
-        # Debugging: Print the recommendations
-        st.write("Top Recommendations:")
-        st.table(recommendations)
-
-    except Exception as e:
-        # Print the exception details for debugging
-        st.write(f"Error: {e}")
-        st.write("Full exception details:")
-        st.write(e)
+# Display the recommendations
+st.write("Top 5 Recommendations:")
+st.table(recommendations)
