51255ec6e8537e5c68f07a68823e322955a9278a
RenatinhaDC <143716639+RenatinhaDC@users.noreply.github.com>
2023-11-09 20:15:35 +0100

    Add files via upload

--- streamlit_a.py
+++ streamlit_a.py
@@ -4,3 +4,8 @@ st.title("WBSFlix Recomender")
 
 st.write("The best movie recomendations for you!")
 
+title= st.text_input("What is your favourite movie")
+
+st.write("Your favourite movie is:", title)
+
+
