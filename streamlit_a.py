2ecc19688e5edbeb608e850844837a855015f0ad
RenatinhaDC <143716639+RenatinhaDC@users.noreply.github.com>
2023-11-09 22:19:22 +0100

    Add files via upload

--- streamlit_a.py
+++ streamlit_a.py
@@ -1,33 +1,39 @@
-
-#Function for streamlit
-import streamlit as st
 import pandas as pd
+import streamlit as st
 
-# Calculate weighted scores and get top movies
+# Load data
+links = pd.read_csv("links.csv")
+movies = pd.read_csv("movies.csv")
+ratings = pd.read_csv("ratings.csv")
+tags = pd.read_csv("tags.csv")
+
+# Merge tables
+movie_ratings = pd.merge(movies, ratings, on='movieId')
+movie_ratings_tags = pd.merge(movie_ratings, tags, on='movieId')
+
+# Popularity Recommender
+rating_count_df = movie_ratings_tags.groupby('movieId')['rating'].agg(['mean', 'count']).reset_index()
+
+# Weighted Score
 rating_w8t = 0.6
 count_w8t = 0.4
-
 rating_count_df['w8ed_score'] = (rating_count_df['mean'] * rating_w8t) + (rating_count_df['count'] * count_w8t)
 
+# Top Movies
 top_movies = rating_count_df.sort_values(by='w8ed_score', ascending=False).head(20)
-
 top_movies_info = movie_ratings_tags[movie_ratings_tags['movieId'].isin(top_movies['movieId'])][movie_info_columns].drop_duplicates()
-
 top_movies_info = top_movies_info.merge(top_movies[['movieId', 'w8ed_score']], on='movieId', how='left')
 
 # Streamlit App
-st.title("Top Movie Recommender")
-st.write("Discover top-rated movies!")
+st.title("WBSFlix Recommender")
+st.write("The best movie recommendations for you!")
 
-# Function to get top recommendations (Input is DataFrame and n)
-def get_top_n(top_movies_info, n):
-    top_n_movies_df = pd.DataFrame(top_movies_info).nlargest(n, "w8ed_score")
-    return top_n_movies_df
+# Display Top Movies
+st.write("Top Movies:")
+st.table(top_movies_info)
 
-# Input for the number of movies to recommend
-n_recommendations = st.number_input("How many movies would you like to recommend?", min_value=1, max_value=10, value=3, step=1)
-
-# Display the top recommendations
-top_n_recommendations = get_top_n(top_movies_info, n_recommendations)
-st.write(f"Top {n_recommendations} Recommendations:")
-st.table(top_n_recommendations)
+# Display Movie Details
+selected_movie = st.selectbox("Select a movie for details:", top_movies_info['title'])
+selected_movie_details = top_movies_info[top_movies_info['title'] == selected_movie]
+st.write("Movie Details:")
+st.table(selected_movie_details)
