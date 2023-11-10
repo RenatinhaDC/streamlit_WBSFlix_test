81090614ba57ce023c1d7d0e5f9b20363e381dc3
RenatinhaDC <143716639+RenatinhaDC@users.noreply.github.com>
2023-11-10 11:05:06 +0100

    Add files via upload

---
+++ streamlit_v_rc.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import streamlit as st
+
+with becouse_you_like:
+    st.header('Similar to')
+    #st.text('Write a name')
+    
+    #selection_col, display_col = st.columns(2)
+    input_feature = str(st.text_input('Movie title'))
+    
+    # py function get sparse matrix
+    def get_sparse_matrix(data: pd.DataFrame): 
+
+         return(user_movie_matrix2 = pd.pivot_table(data=movie_ratings_tags,
+                                  values='rating',
+                                  index='userId_x',
+                                  columns='title',
+                                  fill_value=0)            
+         )
+
+
+
+    # py function item based recommender
+    def item_based_recommender(data: pd.DataFrame, title: str, n: int=5):
+    
+    sparse_matrix = user_movie_matrix2
+        
+    return(
+         sparse_matrix
+             .corrwith(sparse_matrix[title])
+             .sort_values(ascending=False)
+             .index
+             .to_list()[1:n+1]
+         )
+    similar_movies = item_based_recommender(movie_ratings_tags.copy(), input_feature)
+    
+    return(similar_movies)
