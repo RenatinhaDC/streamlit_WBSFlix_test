518637f3bb73ba4f109801998ec5cb72fc15afce
RenatinhaDC <143716639+RenatinhaDC@users.noreply.github.com>
2023-11-10 11:28:50 +0100

    Add files via upload

--- streamlit_v_rc.py
+++ streamlit_v_rc.py
@@ -14,32 +14,33 @@ with becouse_you_like:
     #selection_col, display_col = st.columns(2)
     input_feature = str(st.text_input('Movie title'))
     st.write("Your favorite movie is:", input_feature)
+    
     # py function get sparse matrix
-    #def get_sparse_matrix(data: pd.DataFrame): 
+    def get_sparse_matrix(data: pd.DataFrame): 
 
-        # return(movie_ratings_tags.pivot_table(data=movie_ratings_tags,
-         #                         values='rating',
-         #                         index='userId_x',
-          #                        columns='title',
-           #                       fill_value=0)            
-         #)
+         return(movie_ratings_tags.pivot_table(data=movie_ratings_tags,
+                                  values='rating',
+                                  index='userId_x',
+                                  columns='title',
+                                  fill_value=0)            
+         )
 
 
 
     # py function item based recommender
-    #def item_based_recommender(data: pd.DataFrame, title: str, n: int=5):
+    def item_based_recommender(data: pd.DataFrame, title: str, n: int=5):
     
-     #   sparse_matrix = get_sparse_matrix(movie_ratings_tags)
+        sparse_matrix = get_sparse_matrix(movie_ratings_tags)
         
-   #     return(
-       #      sparse_matrix
-        #         .corrwith(sparse_matrix[title])
-         #        .sort_values(ascending=False)
-          #       .index
-           #      .to_list()[1:n+1]
-            # )
-    #similar_movies = item_based_recommender(movie_ratings_tags.copy(), input_feature)
+        return(
+             sparse_matrix
+                 .corrwith(sparse_matrix[title])
+                 .sort_values(ascending=False)
+                 .index
+                 .to_list()[1:n+1]
+             )
+    similar_movies = item_based_recommender(movie_ratings_tags.copy(), input_feature)
     
     
 
-    #st.dataframe(similar_movies)
+    st.dataframe(similar_movies)
