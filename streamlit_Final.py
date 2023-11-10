5a7e8ed18065c28dfd7b36ab71ca817c9f3fce23
RenatinhaDC <143716639+RenatinhaDC@users.noreply.github.com>
2023-11-10 13:41:45 +0100

    Update streamlit_Final.py

--- streamlit_Final.py
+++ streamlit_Final.py
@@ -5,6 +5,19 @@ import streamlit as st
 import base64
 
 
+import numpy as np
+
+sample_rate = 44100  # 44100 samples per second
+seconds = 2  # Note duration of 2 seconds
+
+frequency_la = 440  # Our played note will be 440 Hz
+
+# Generate array with seconds*sample_rate steps, ranging between 0 and seconds
+t = np.linspace(0, seconds, seconds * sample_rate, False)
+
+# Generate a 440 Hz sine wave
+note_la = np.sin(frequency_la * t * 2 * np.pi)
+st.audio(note_la, sample_rate=sample_rate)
 
 st.markdown("![Alt Text](https://media.giphy.com/media/KpACNEh8jXK2Q/giphy.gif)")
 
